9e7a46e69fe64ec60693bf778c03582edcd6b8a6
TooIneptNeedToLearn <moltencore23@gmail.com>
2023-05-27 01:19:23 +0800

    "Added PUT and DELETE function, improved naming"

--- api.py
+++ api.py
@@ -6,12 +6,12 @@ from flask import jsonify, request, flash, render_template
 #READ
 #index that shows the basics of this program
 @app.route('/')
-def index():
+def read_index():
     return render_template("intro.html")
 
 #route that shows all the table
 @app.route('/tables')
-def tables():
+def read_tables():
     conn = None
     cursor = None
     try:
@@ -31,7 +31,7 @@ def tables():
 
 #route that shows a specific row in the table
 @app.route('/tables/<int:customers_id>')
-def selected_row(customers_id):
+def read_selected_row(customers_id):
     conn = None
     cursor = None
     try: 
@@ -51,7 +51,7 @@ def selected_row(customers_id):
 
 #POST
 @app.route('/create', methods = ['POST'])
-def add_input():
+def create_add_input():
     conn = None
     cursor = None
     try:
@@ -76,6 +76,61 @@ def add_input():
     finally:
         cursor.close()
         conn.close()
+
+#PUT
+@app.route('/update', methods = ['PUT'])
+def update_row():
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        customers_id = _json['customers_id']
+        f_name = _json['f_name']
+        l_name = _json['l_name']
+        if customers_id and f_name and l_name and request.method == 'PUT':
+            sqlquerry = "update customers set f_name = %s, l_name = %s where customers_id = %s"
+            bind_var = (f_name, l_name, customers_id)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sqlquerry,bind_var)
+            conn.commit()
+            response = jsonify(f"row {customers_id} values updated successfully")
+            response.status_code = 200
+            return response
+        else:
+            errormess()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+#DELETE
+@app.route('/delete/<int:customers_id>', methods=['DELETE'])
+def delete_row(customers_id):
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sqlquery = "delete from customers where customers_id = %s"
+        cursor.execute(sqlquery,(customers_id,))
+        conn.commit()
+        respone = jsonify(f'row {customers_id} deleted successfully!')
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print(e)
+    finally:
+       cursor.close()
+       conn.close()
+    
+
+
+
+
+
 
 #error handler that will print a message if you got 404
 @app.errorhandler(404)
